660bc6a6cc509ab2e3b9829974b0f1816ffadf8b
evheny0 <evheny0@gmail.com>
2013-12-01 23:46:21 +0300

    Added email comfirmation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130233137) do
+ActiveRecord::Schema.define(version: 20131127193339) do
 
   create_table "chapters", force: true do |t|
     t.string   "title"
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 20131130233137) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
